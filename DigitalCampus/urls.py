5bb8b56280ef428c408f57b37e3ec6036c82b1b4
abfembest <abfembest>
2026-01-07 23:05:52 +0000

    The app was created

--- DigitalCampus/urls.py
+++ DigitalCampus/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for digicam project.
+URL configuration for DigitalCampus project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('', include('eduweb.urls')),
 ]
+
+

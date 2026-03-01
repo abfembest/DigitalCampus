c25d90f541ce234cc4df0b841ed8f812e2c4ccf6
abfembest <abfembest>
2026-03-01 11:22:52 +0000

    Some pages are not responsive across other browsers, this was sorted

--- DigitalCampus/settings.py
+++ DigitalCampus/settings.py
@@ -37,7 +37,7 @@ DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default="127.0.0.1,localhost",
+    default="127.0.0.1, localhost, www.miuedu.com, miuedu.com,https://www.miuedu.com",
     cast=lambda v: [h.strip() for h in v.split(",")]
 )
 

54ea883405c9d12e56a9d7de7475fe309d815400
abfembest <abfembest>
2026-03-31 21:54:57 +0100

    I updated the setting CORS

--- DigitalCampus/settings.py
+++ DigitalCampus/settings.py
@@ -225,6 +225,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://www.miuedu.com",
     "http://127.0.0.1",
     "http://localhost",
+    "https://www.theology.miuedu.com"
 ]
 
 CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS
